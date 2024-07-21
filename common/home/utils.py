ab7fb72b8383c6a068509d4c1187272847b1bf9f
young-hun-jo <joyh951021@gmail.com>
2024-07-21 20:55:56 +0900

    test

--- common/home/utils.py
+++ common/home/utils.py
@@ -1,5 +1,8 @@
-import mlflow
+import argparse
 import subprocess
+
+import bentoml
+import mlflow
 
 
 # ref: https://stackoverflow.com/questions/63525498/how-do-i-set-a-different-local-directory-for-mlflow
@@ -11,3 +14,22 @@ def get_git_root_path() -> str:
     cmd = "git rev-parse --show-toplevel"
     git_root_path: str = subprocess.check_output(cmd, shell=True).decode("utf-8")
     return git_root_path
+
+
+def transplant_mlflow_model_to_bentoml_model_store(args: argparse.Namespace) -> None:
+    _ = bentoml.mlflow.import_model(
+        name=args.bentoml_model_name,
+        model_uri=args.model_uri
+    )
+
+
+def transplant_mlflow_model() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--bentoml_model_name", type=str, required=True)
+    parser.add_argument("--model_uri", type=str, required=True)
+    args = parser.parse_args()
+    transplant_mlflow_model_to_bentoml_model_store(args)
+
+
+if __name__ == "__main__":
+    transplant_mlflow_model()
