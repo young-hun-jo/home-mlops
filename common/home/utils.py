b820d48357ab8307a142dc44a522851c6d16395b
young-hun-jo <joyh951021@gmail.com>
2024-07-21 20:58:29 +0900

    test

--- common/home/utils.py
+++ common/home/utils.py
@@ -31,5 +31,9 @@ def transplant_mlflow_model() -> None:
     transplant_mlflow_model_to_bentoml_model_store(args)
 
 
+def greet():
+    print("Hello console script in greet!")
+
+
 if __name__ == "__main__":
-    transplant_mlflow_model()
+    greet()
